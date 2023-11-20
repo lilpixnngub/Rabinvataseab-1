38a7328eb483771718c77f36c39a82746a863406
noppasornswnk <noppasornsuw@gmail.com>
2023-11-21 03:48:39 +0700

    forgotpass,login validate

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const connection = mysql.createPool({
 const bodypaser = require('body-parser');
 
 app.use(bodypaser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'Rabinvataseab')));
+app.use(express.static(path.join(__dirname)));
 app.use(express.static('View/Routes/stylesheet'));
 
 // Serve the HTML file for the signup page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Rabinvataseab', 'View', 'Routes', 'SignupPage.html'));
+    res.sendFile(path.join(__dirname, 'View', 'Routes', 'SignupPage.html'));
 });
 
 app.post('/signup', (req, res) => {
@@ -46,32 +46,58 @@ app.post('/signup', (req, res) => {
 });
 
 app.get('/start', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Rabinvataseab', 'View', 'Routes', 'StartPage.html'));
+  res.sendFile(path.join(__dirname, 'View', 'Routes', 'StartPage.html'));
 })
 app.get('/log', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'Rabinvataseab', 'View', 'Routes','LoginPage.html'));
+    res.sendFile(path.join(__dirname, 'View', 'Routes','LoginPage.html'));
 })
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname,'Rabinvataseab', 'View', 'Routes', 'HomePage.html'))
+    res.sendFile(path.join(__dirname, 'View', 'Routes', 'HomePage.html'));
 })
+app.get('/forgot', (req, res) => {
+    res.sendFile(path.join(__dirname, 'View', 'Routes', 'ForgotpassPage.html'));
+})
+
 app.post('/login', (req, res) => {
   const { Email, Password } = req.body;
 
-  // Query to retrieve hashed password from the database based on the provided username
+  // Query to retrieve hashed password from the database based on the provided email
   const SELECT_USER_QUERY = 'SELECT Password FROM Member WHERE Email = ?';
 
   connection.query(SELECT_USER_QUERY, [Email], (error, results, fields) => {
     if (error) {
       res.status(500).json({ message: 'Error retrieving data from the database' });
     } else {
-      if(results.length>0) {
-        res.redirect('/start')
+      if (results.length > 0) {
+        const hashedPasswordFromDB = results[0].Password;
+
+        // Compare the provided password with the hashed password
+        if (hashedPasswordFromDB === Password) {
+          res.redirect('/start');
+        } else {
+          res.send('Invalid password');
+        }
+      } else {
+        res.send('User not found');
       }
-  }
+    }
+  });
 });
-})
-app.post('/homepage', (req, res) => {
-  res.redirect('/home');
-})
+
+app.post('/forgotpassword', (req, res) => {
+  const { Email, Password } = req.body;
+
+  const UPDATE_PASSWORD_QUERY = 'UPDATE Member SET Password = ? WHERE Email = ?';
+
+  connection.execute(UPDATE_PASSWORD_QUERY, [Password, Email], (err) => {
+    if (err) {
+      console.error('Password update error:', err);
+      res.send('Error updating password');
+    } else {
+      res.status(200).send('Password reset successfully');
+      res.redirect('/home');
+    }
+  });
+});
 
 app.listen(3000, ()=> console.log('Server is running on port 3000'));
